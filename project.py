d7ec94a6a49ed926ec932cb64e7c600df3b09dfb
Timothy Lui <timm@Timothys-MacBook-Air.local>
2018-11-16 17:55:19 -0500

    new code for reading packet traces

--- project.py
+++ project.py
@@ -4,12 +4,13 @@ import dpkt
 
 counter=0
 ipcounter=0
+ethcounter=0
+ipv6counter=0
 icmpcounter=0
 tcpcounter=0
 udpcounter=0
-ethcounter=0
 
-filename='univ1_pt8.pcap'
+filename='tests/test2.pcap'
 
 for ts, pkt in dpkt.pcap.Reader(open(filename,'r')):
 
@@ -26,10 +27,20 @@ for ts, pkt in dpkt.pcap.Reader(open(filename,'r')):
 
     if ip.p==dpkt.ip.IP_PROTO_UDP:
        udpcounter+=1
+   
+    if ip.p==dpkt.ip.IP_PROTO_IP6: 
+       ipv6counter+=1
+       
+    if ip.p==dpkt.ip.IP_PROTO_ICMP: 
+       icmpcounter+=1
+
+    if ip.p==dpkt.ip.IP_PROTO_ETHERIP: 
+       ethcounter+=1
 
 print "Total number of packets in the pcap file: ", counter
 print "Total number of ip packets: ", ipcounter
+print "Total number of ipv6 packets: ", ipv6counter
 print "Total number of icmp packets: ", icmpcounter
 print "Total number of tcp packets: ", tcpcounter
 print "Total number of udp packets: ", udpcounter
-print "Total number of ethernet packets: ", ethcounter
+print "Total number of eth packets: ", ethcounter
