19be0ebf81717a3cf7bdb57cb99e0ae8a318d8cc
Timothy Lui <timm@Timothys-MacBook-Air.local>
2018-11-20 17:41:53 -0500

    Working analysis of trace file for packet #'s

--- details.py
+++ details.py
@@ -43,15 +43,14 @@ def print_packets(pcap):
     eth_counter=0
     arp_counter=0
 
-    ip_counter=0
+    ipv4_counter=0
     ipv6_counter=0
     icmp_counter=0
-    icmp_counter2=0
+    network_other_counter=0
 
     tcp_counter=0
     udp_counter=0
-
-    other_counter=0
+    transport_other_counter=0
 
     not_ip=0
 
@@ -61,42 +60,36 @@ def print_packets(pcap):
         # Total count of packets
         counter+=1
 
-        # if isinstance(buf,dpkt.ethernet.ETH_TYPE_IP):
-        #     print "reach 1"
-        if type(buf) == dpkt.ethernet.ETH_TYPE_IP6:
-            print "reach 2"
-        
-
         # Unpack the Ethernet frame (mac src/dst, ethertype)
         eth = dpkt.ethernet.Ethernet(buf)
         #print('Ethernet Frame: ', mac_addr(eth.src), mac_addr(eth.dst), eth.type)
 
-        if eth.type != dpkt.ethernet.ETH_TYPE_IP:
-            if eth.type == dpkt.ethernet.ETH_TYPE_IP6:
-                ipv6_counter+=1
-                break
-            elif eth.type == dpkt.ethernet.ETH_TYPE_ARP:
-                arp_counter+=1
-            else:
-                other_counter+=1
-            #continue
-        
         eth_counter+=1
 
-        # Make sure the Ethernet data contains an IP packet
-        if not isinstance(eth.data, dpkt.ip.IP):
-            print('Non IP Packet type not supported %s\n' % eth.data.__class__.__name__)
-            not_ip+=1
+        if eth.type == dpkt.ethernet.ETH_TYPE_IP:
+            ipv4_counter+=1
+        elif eth.type == dpkt.ethernet.ETH_TYPE_IP6:
+            ipv6_counter+=1
+        elif eth.type == dpkt.ethernet.ETH_TYPE_ARP:
+            arp_counter+=1
             continue
+        else:
+            network_other_counter+=1
+            continue
+
+        # Make sure the Ethernet data contains an IP packet
+        # if not isinstance(eth.data, dpkt.ip.IP):
+        #     print('Non IP Packet type not supported %s\n' % eth.data.__class__.__name__)
+        #     not_ip+=1
+        #     continue
 
         # Now unpack the data within the Ethernet frame (the IP packet)
         # Pulling out src, dst, length, fragment info, TTL, and Protocol
         ip = eth.data
-        ip_counter+=1
 
         # Now check if this is an ICMP packet
         if isinstance(ip.data, dpkt.icmp.ICMP):
-            icmp_counter2+=1
+            icmp_counter+=1
 
         # Pull out fragment information (flags and offset all packed into off field, so use bitmasks)
         # do_not_fragment = bool(ip.off & dpkt.ip.IP_DF)
@@ -106,38 +99,38 @@ def print_packets(pcap):
         # Print out the info
         # print('IP: %s -> %s   (len=%d ttl=%d DF=%d MF=%d offset=%d)\n' % \
         #       (inet_to_str(ip.src), inet_to_str(ip.dst), ip.len, ip.ttl, do_not_fragment, more_fragments, fragment_offset))        
-        
-        if ip.p == dpkt.ip.IP_PROTO_ICMP6:
-            ipv6_counter+=1
-
-        if ip.p == dpkt.ip.IP_PROTO_ICMP: 
-            icmp_counter+=1
 
         if ip.p == dpkt.ip.IP_PROTO_TCP: 
             tcp_counter+=1
-
-        if ip.p == dpkt.ip.IP_PROTO_UDP:
+        elif ip.p == dpkt.ip.IP_PROTO_UDP:
             udp_counter+=1
+        else:
+            transport_other_counter+=1
     
 
     print "Total number of packets in the pcap file: ", counter
 
+    print "-- Data Link Layer --"
     print "Total number of eth packets: ", eth_counter
     print "Total number of arp packets: ", arp_counter
 
-    print "Total number of ipv4 packets: ", ip_counter
+    print "-- Network Layer --"
+    print "Total number of ipv4 packets: ", ipv4_counter
     print "Total number of ipv6 packets: ", ipv6_counter
     print "Total number of icmp packets: ", icmp_counter
+    print "Total number of other packets:", network_other_counter
     
+    print "-- Transport Layer --"
     print "Total number of tcp packets: ", tcp_counter
     print "Total number of udp packets: ", udp_counter
+    print "Total number of other packets:", transport_other_counter
     
-    total_equal = other_counter + arp_counter
-    equal = total_equal == not_ip
-    print("Are protocols Other and not Ethernet/ARP equal? %d vs %d: %s" % (total_equal, not_ip, equal))
+    # total_equal = other_counter + arp_counter
+    # equal = total_equal == not_ip
+    # print("Are protocols Other and not Ethernet/ARP equal? %d vs %d: %s" % (total_equal, not_ip, equal))
 
-    equal2 = icmp_counter == icmp_counter2
-    print("Are ICMP counters equal? %d vs %d: %s" % (icmp_counter, icmp_counter2, equal2))
+    # equal2 = icmp_counter == icmp_counter2
+    # print("Are ICMP counters equal? %d vs %d: %s" % (icmp_counter, icmp_counter2, equal2))
 
 
 def test():
